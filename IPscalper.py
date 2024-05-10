bd3e53874113e4006aa1bde8c139e98ef2895651
pbwbb <151950149+pbwbb@users.noreply.github.com>
2024-05-10 13:24:32 -0300

    Add files via upload

---
+++ IPscalper.py
@@ -0,0 +1,375 @@
+#!usr/bin/python
+import requests
+import argparse
+import json
+import urllib3
+
+
+#pip install requests
+#pip install argparse
+#pip install json
+
+with open('API_keys.txt', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        if line.startswith("VirusTotal"):
+            VirusTotal_key = line.split(':')[1].strip()
+        if line.startswith("AbuseIPdb"):
+            AbuseIPdb_key = line.split(':')[1].strip()
+        if line.startswith("OTX"):
+            otx_key = line.split(':')[1].strip()
+        if line.startswith("CriminalIP"):
+            CriminalIP_key = line.split(':')[1].strip()
+        if line.startswith("GreyNoise"):
+            GreyNoise_key = line.split(':')[1].strip()
+
+# Change API keys here
+# AbuseIPdb_key = ""    
+# VirusTotal_key = ""
+# otx_key = ""
+# CriminalIP_key
+
+
+def logo():
+    ascii = '''
+    ooOoOOo OooOOo.                      o                    
+       O    O     `O                    O                     
+       o    o      O                    o                     
+       O    O     .o                    O                     
+       o    oOooOO'  .oOo  .oOo  .oOoO' o  .oOo. .oOo. `OoOo. 
+       O    o        `Ooo. O     O   o  O  O   o OooO'  o     
+       O    O            O o     o   O  o  o   O O      O     
+    ooOOoOo o'       `OoO' `OoO' `OoO'o Oo oOoO' `OoO'  o     
+                                           O                  
+                                           o'  by Pedro Webber                
+    '''
+    print(ascii)
+    linha_separacao()
+    # I used this https://manytools.org/hacker-tools/ascii-banner/ to make the logo :)
+
+def linha_separacao():
+    print("\n"+("-+" * 10))
+
+def ip_api(IP,args):
+    try:
+        response = requests.get(f"http://ip-api.com/json/{IP}")
+        data = response.json()
+        if args.raw == True:
+             print(response.text)
+        elif data['status'] == "success":
+            print("\nIP location:\n")
+            print(f"country: {data['country']}({data['countryCode']})")
+            print(f"region: {data['regionName']}({data['region']})")
+            print(f"city: {data['city']}")
+            print(f"zip: {data['zip']}")
+            print(f"coordinates: lat: {data['lat']} lon: {data['lon']}")
+            print(f"timezone: {data['timezone']}")
+            linha_separacao()
+        else: print(f"\nIP location:\nHTTP {response.status_code}\n{response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"\nIP location:\n")
+        print("Oops...", e)
+        linha_separacao()
+
+def vt(IP,args,VirusTotal_key):
+    try:
+        headers = {"x-apikey":  VirusTotal_key}
+        response = requests.get(f"https://www.virustotal.com/api/v3/ip_addresses/{IP}", headers=headers)
+        data = response.json()
+        if args.raw == True:
+            print(response.text)
+        elif response.status_code == 200:    
+            analisys = data['data']['attributes']['last_analysis_results']
+            detected = False
+            for vendor in analisys.keys():
+                if analisys[vendor]['result'] not in ["clean","unrated"]:
+                    detected = True 
+            if detected == True:
+                print("\nVirusTotal analisys:\n")
+                print(f"IP: {data['data']['id']} ")
+                print(f"AS Owner: {data['data']['attributes']['as_owner']}")
+                print(f"votes: \n\tharmless: {data['data']['attributes']['total_votes']['harmless']}\n\tmalicious: {data['data']['attributes']['total_votes']['malicious']}")
+                for vendor in analisys.keys():
+                    if analisys[vendor]['result'] not in ["clean","unrated"]:
+                        print(f"Vendor: {analisys[vendor]['engine_name']}")
+                        print(f"Category: {analisys[vendor]['category']}")
+                        print(f"Result: {analisys[vendor]['result']}")
+                linha_separacao()
+            elif detected == False:
+                print("VirusTotal analisys:\n")
+                print(f"IP: {data['data']['id']} ")
+                print(f"AS Owner: {data['data']['attributes']['as_owner']}")
+                print(f"votes: \n\tharmless: {data['data']['attributes']['total_votes']['harmless']}\n\tmalicious: {data['data']['attributes']['total_votes']['malicious']}")
+                print("No threats detected")
+                linha_separacao()
+        elif response.status_code == 401:
+                print("\nVirusTotal:\n")
+                print(f"Ivalid API key\nMessage: {data['error']['message']}")
+                linha_separacao()
+        else: print(f"\nVirusTotal:\nHTTP {response.status_code}\n{response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"\nVirusTotal:\n")
+        print("Oops...", e)
+        linha_separacao()
+
+def abuseIP(IP,args,AbuseIPdb_key):
+    try:
+        url = 'https://api.abuseipdb.com/api/v2/check'
+        querystring = {'ipAddress': IP,'maxAgeInDays': '90'}
+        headers = {'Accept': 'application/json','Key': AbuseIPdb_key}
+        response = requests.request(method='GET', url=url, headers=headers, params=querystring)
+        data = response.json()
+        if args.raw == True: 
+            print(response.text)
+        elif response.status_code == 200:
+            print("\nAbuseIPdb:\n")
+            print(f"IP: {data['data']['ipAddress']}")
+            print(f"Domain: {data['data']['domain']}")
+            for hostname in data['data']['hostnames']:
+                print(f"hostnames: {hostname}")
+            print(f"Abuse Confidence Score: {data['data']['abuseConfidenceScore']}")
+            print(f"IP public: {data['data']['isPublic']}")
+            print(f"IP version: {data['data']['ipVersion']}")
+            print(f"Whitelisted: {data['data']['isWhitelisted']}")
+            linha_separacao()
+        elif response.status_code == 401:
+                print("\nAbuseIPdb:\n")
+                print(f"Ivalid API key\nMessage: {data['errors']}")
+                linha_separacao()
+        else: print(f"\nAbuseIPdb:\nHTTP {response.status_code}\n{response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"\nAbuseIPdb:\n")
+        print("Oops...", e)
+        linha_separacao() 
+
+def otx(IP,args,otx_key): 
+    try:
+        url = f'https://otx.alienvault.com/api/v1/indicators/IPv4/{IP}/general'
+        headers = {'X-OTX-API-KEY' : otx_key}
+        response = requests.request(method='GET', url=url, headers=headers)
+        data = response.json()
+        if args.raw == True: 
+            print(response.text)
+        elif response.status_code == 200:
+            print("\nOTX AlienVault:\n")
+            print(f"IP: {data['indicator']}")
+            print(f"Reputation: {data['reputation']}")
+            print(f"Pulses: {data['pulse_info']['count']}")
+            for pulse in data['pulse_info']['pulses']:
+                print(f"Name: {pulse['name']}")
+                print("tags:")
+                for i in pulse['tags']: print("\t",i)
+            if len(data['pulse_info']['related']['alienvault']['malware_families']) > 0:
+                print("Malware families(alienvault):")
+                for i in data['pulse_info']['related']['alienvault']['malware_families']: print("\t",i)
+            if len(data['pulse_info']['related']['other']['malware_families']) > 0:
+                print("Malware families(other):")
+                for i in data['pulse_info']['related']['other']['malware_families']: print("\t",i)    
+            linha_separacao()
+        elif response.status_code == 401:
+                print("\nOTX AlienVault:\n")
+                print(f"Ivalid API key\n")
+                linha_separacao()
+        else: print(f"\nOTX AlienVault:\nHTTP {response.status_code}\n{response.text}")          
+    except requests.exceptions.RequestException as e:
+        print(f"\nOTX AlienVault:\n")
+        print("Oops...", e)
+        linha_separacao()
+
+def crimninalip(IP,args,CriminalIP_key):
+    try:
+        url = (f'https://api.criminalip.io/v1/feature/ip/malicious-info?ip={IP}')
+        headers = {'x-api-key': CriminalIP_key}
+        response = requests.request(method='GET', url=url, headers=headers)
+        data = response.json()
+        if args.raw == True: 
+            print(response.text)
+        elif data['status'] == 200:       
+            print(f"\nCriminalIP:\n")
+            print(f"IP: {data['ip']}")
+            print(f"Malicious: {data['is_malicious']}")
+            print(f"VPN: {data['is_vpn']}")
+            print(f"Remote Access: {data['can_remote_access']}")
+            print(f"Open Ports: {data['current_opened_port']['count']}")
+            if data['current_opened_port']['count'] > 0:
+                for i in data['current_opened_port']['data']:
+                    print(f"\tPort: {i['port']} {i['socket_type']}")
+                    print(f"\tProtocol: {i['protocol']}\n")
+            print(f"Vulnerabilities: {data['vulnerability']['count']} {data['vulnerability']['data']}")
+            linha_separacao()
+        elif data['status'] == 500:
+            print("\nCriminalIP:\n")
+            print(f"Ivalid API key\nmessage:{data['message']}\nStatus: {data['status']}")
+            linha_separacao()
+        else: print(f"\nCriminalIP:\nHTTP {data['status']}\n{response.text}"), linha_separacao() 
+    except requests.exceptions.RequestException as e:
+        print(f"\nCriminalIP:\n")
+        print("Oops...", e)
+        linha_separacao()
+
+def C2(IP,args):
+    try:
+        response = requests.get("https://feodotracker.abuse.ch/downloads/ipblocklist.json")
+        data = response.json()
+        detected = False
+        if response.status_code == 200:
+            for c2 in data:
+                if IP in c2['ip_address']:
+                    detected = True
+            if detected == True:
+                for c2 in data:
+                    if args.raw ==True: 
+                        if IP in c2['ip_address']:
+                            print(c2)
+                        break
+                    else:
+                        if IP in c2['ip_address']:
+                            print(f"\nFEODO C2 tracker by abuse.ch:\n")
+                            print(f"IP: {c2['ip_address']}")
+                            print(f"Malware: {c2['malware']}")
+                            print(f"Port: {c2['port']}")
+                            print(f"Status: {c2['status']}")
+                            print(f"Hostname: {c2['hostname']}")
+                            print(f"Country: {c2['country']}")
+                            print(f"Last online: {c2['last_online']}") 
+                            linha_separacao()    
+            if detected == False:
+                if args.raw == False:    
+                    print(f"\nFEODO c2 tracker by abuse.ch:\n")
+                    print(f"Not found on the past 30 days IOCs") 
+                    linha_separacao()
+        else: print(f"\nFEODO C2 tracker by abuse.ch:\nHTTP{response.status_code}\n{response.text}")
+    except requests.exceptions.RequestException as e:
+      print(f"\nFEODO C2 tracker by abuse.ch:\n")
+      print("Oops...", e)
+      linha_separacao()
+
+def threatfox(IP,args):
+    try:
+        
+        pool = urllib3.HTTPSConnectionPool('threatfox-api.abuse.ch', port=443, maxsize=50)
+        query = {
+            'query':            'search_ioc',
+            'search_term':      IP
+        }
+        json_query = json.dumps(query)
+        response = pool.request("POST", "/api/v1/", body=json_query)
+        data = response.json()
+        if args.raw == True: 
+            print(response)
+        elif data['query_status'] == 'no_result':
+            print("\nThreatFox by abuse.ch:\n")
+            print(data['data'])
+            linha_separacao()    
+        elif data['query_status'] == 'ok':
+            print("\nThreatFox by abuse.ch:\n")
+            print(f"ID: {data['data'][0]['id']}")
+            print(f"IoC: {data['data'][0]['ioc']}")
+            print(f"Threat type: {data['data'][0]['threat_type']}")
+            print(f"Malware: {data['data'][0]['malware_printable']} ({data['data'][0]['malware']})")
+            print(f"Confidence: {data['data'][0]['confidence_level']}")
+            print(f"First seen: {data['data'][0]['first_seen']}")
+            print(f"Last seen: {data['data'][0]['last_seen']}")
+            linha_separacao() 
+        else: print(f"\nThreatFox by abuse.ch:\nHTTP {response.status_code}\n{response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"\nThreatFox by abuse.ch:\n")
+        print("Oops...", e)
+        linha_separacao()
+
+def greynoise(IP,args,Greynoise_key):
+    try:
+        url = (f'https://api.greynoise.io/v3/community/{IP}')
+        headers = {'key': Greynoise_key}
+        response = requests.request(method='GET', url=url, headers=headers)
+        data = response.json()
+        if args.raw == True: 
+            print(response.text)
+        elif response.status_code == 200:
+            print("\nGreyNoise:\n")
+            print(f"GreyNoise: {data['ip']}")
+            print(f"Name: {data['name']}") 
+            if data['riot'] == True: print("This IP is part of our RIOT project, which identifies IPs from known benign services and organizations that commonly cause false positives in network security and threat intelligence products.")
+            elif data['noise'] == True: print(f"Ths IP has been seen scanning the internet\nLast seen: {data['last_seen']}")    
+            linha_separacao()
+        elif response.status_code == 401:
+            print("\nGreyNoise:\n")
+            print(f"Ivalid API key\n")
+            linha_separacao()
+        elif response.status_code == 404:
+            print("\nGreyNoise:\n")
+            print(f"IP: {data['ip']}")
+            print(data['message'])        
+        else: print(f"\nOTX AlienVault:\nHTTP {response.status_code}\n{response.text}")          
+    except requests.exceptions.RequestException as e:
+        print(f"\nGreyNoise:\n")
+        print("Oops...", e)
+        linha_separacao()
+
+def main(): 
+    description = (f"A tool for finding open source information about IP addresses\n All tools used on this script are free, although API keys are necessary for some of them. To get the keys you only need to create an account.\nBe mindiful of API limits.\nI am not responsible for any misuse of APIs or tools on this script")
+    parser=argparse.ArgumentParser(prog="IPscalper",description=description,epilog="https://github.com/pbwbb/Projects/tree/main/IPscalper by Pedro Webber")
+    parser.add_argument("IP", help="IP address that is going to be searched")
+    parser.add_argument("-all", required=False,  action="store_true", help="Uses all tools")
+    # parser.add_argument("-v","--verbose", required=False,  action="store_true", help="Verbose output")
+    parser.add_argument("-geo","--location", required=False,  action="store_true", help="Uses IP-api to get IP location (no key needed)") #IP-API no key
+    parser.add_argument("-raw", required=False, action="store_true", help="Displays raw json output")
+    parser.add_argument("-vt","--VirusTotal", required=False,  action="store_true", help="Uses VirusTotal API for info (key required -> edit API_keys.txt file or uncomment lines)")
+    parser.add_argument("-abuse","--AbuseIPdb", required=False,  action="store_true", help="Uses AbuseIPdb API for info (key required -> edit API_keys.txt file or uncomment lines)")  
+    parser.add_argument("-otx","--AlienVault", required=False,  action="store_true", help="Uses OTX AlienVault API for info (key required -> edit API_keys.txt file or uncomment lines)") # alienvault
+    parser.add_argument("-showkeys", required=False,  action="store_true", help="display API keys") # display API keys
+    parser.add_argument("-criminalip", required=False,  action="store_true", help="Uses CriminalIP api for info (key required -> edit API_keys.txt file or uncomment lines)")
+    parser.add_argument("-c2","--Feodo", required=False,  action="store_true", help="Checks if IP is in Abuse.ch Feodo tracker last 30 days C2 IoCs (no key needed)") #https://feodotracker.abuse.ch
+    parser.add_argument("-threatfox", required=False,  action="store_true", help="Uses abuse.ch ThreatFox API for info (no key needed)")
+    parser.add_argument("-gn","--greynoise", required=False,  action="store_true", help="Uses GreyNoise API for info (key required -> edit API_keys.txt file or uncomment lines)")
+    parser.add_argument("-nokey", required=False,  action="store_true", help="Only uses tools that do not require an API key")
+    args=parser.parse_args()
+    IP = args.IP
+    if args.all == True:
+        logo()
+        ip_api(IP,args)
+        abuseIP(IP,args,AbuseIPdb_key)
+        crimninalip(IP,args,CriminalIP_key)
+        vt(IP,args,VirusTotal_key)
+        otx(IP,args,otx_key) 
+        C2(IP,args) 
+        threatfox(IP,args)
+        greynoise(IP,args,GreyNoise_key)      
+    else:
+        logo()
+        if args.showkeys == True:
+            print("API keys:\n")
+            print("VirusTotal: ", VirusTotal_key)
+            print("AbuseIPdb: ", AbuseIPdb_key)
+            print("OTX AlienVault: ", otx_key)
+            print("CriminalIP: ", CriminalIP_key)
+        if args.nokey == True:
+            ip_api(IP,args)
+            C2(IP,args)
+            threatfox(IP,args)
+        if args.location == True:
+            ip_api(IP,args)
+        if args.AbuseIPdb == True:
+            abuseIP(IP,args,AbuseIPdb_key)
+        if args.criminalip ==  True:
+            crimninalip(IP,args,CriminalIP_key)
+        if args.VirusTotal == True:
+            vt(IP,args,VirusTotal_key)
+        if args.AlienVault == True:
+            otx(IP,args,otx_key)
+        if args.Feodo == True:
+            C2(IP,args)
+        if args.threatfox == True:
+            threatfox(IP,args)
+        if args.greynoise == True:
+            greynoise(IP,args,GreyNoise_key)
+        if not any(vars(args).values()):
+            ip_api(IP,args)
+            C2(IP,args)
+            threatfox(IP,args)
+   
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ readme.md
@@ -0,0 +1,209 @@
+# IPscalper
+```
+    ooOoOOo OooOOo.                      o                    
+       O    O     `O                    O                     
+       o    o      O                    o                     
+       O    O     .o                    O                     
+       o    oOooOO'  .oOo  .oOo  .oOoO' o  .oOo. .oOo. `OoOo. 
+       O    o        `Ooo. O     O   o  O  O   o OooO'  o     
+       O    O            O o     o   O  o  o   O O      O     
+    ooOOoOo o'       `OoO' `OoO' `OoO'o Oo oOoO' `OoO'  o     
+                                           O                  
+                                           o'  by Pedro Webber                
+    
+```
+## Description
+
+IPscalper is a tool for gathering open-source information about IP addresses. It integrates various APIs to retrieve data such as geographical location, reputation, IoCs and open ports.
+
+You can use this Script to get parsed or raw results from these tools:
+* VirusTotal  
+* AbuseIPdb 
+* OTX ALienVault 
+* CriminalIP 
+* IP-api location
+* Feodo tracker by abuse.ch
+* GreyNoise
+* ThreatFox by abuse.ch
+## Requirements
+
+- Python 3
+- API keys for certain features 
+
+## API keys
+* All APIs used in this script are free although some require you to create an account in order to get a key
+* Read the documentation and be aware of the usage limits of each tool
+
+Tools that require API keys
+
+* VirusTotal (https://docs.virustotal.com/reference/overview) 
+* AbuseIPdb (https://www.abuseipdb.com/api.html)
+* OTX ALienVault (https://otx.alienvault.com/assets/static/external_api.html)
+* CriminalIP (https://www.criminalip.io/developer/api-integrations)
+* GreyNoise (https://docs.greynoise.io/reference/get_v3-community-ip)
+
+
+## Installation
+
+```
+pip install requests argparse urllib3
+````
+Edit the "API_keys.txt" and input the API keys if you wish to use the tools that require them, insert as "Tool:key" do not use "" on the keys
+
+Example API_keys.txt:
+```
+VirusTotal:494e56414c49445f4150495f4b45595f31323334353637383930
+AbuseIPdb:494e56414c49445f4150495f4b45595f31323334353637383930
+OTX:494e56414c49445f4150495f4b45595f31323334353637383930
+CriminalIP:494e56414c49445f4150495f4b45595f31323334353637383930
+GreyNoise:494e56414c49445f4150495f4b45595f31323334353637383930
+```
+## usage
+```
+python IPscalper.py [options] <IP address>
+```
+* Not providing any options will be the same as using the -nokey option
+## help menu
+
+```
+usage: IPscalper [-h] [-all] [-geo] [-raw] [-vt] [-abuse] [-otx] [-showkeys] [-criminalip] [-c2] [-threatfox] [-gn] [-nokey] IP
+
+A tool for finding open source information about IP addresses 
+All tools used on this script are free, although API keys are necessary for some of them. To get the keys you only need to create an       
+account. 
+Be mindiful of API limits. 
+I am not responsible for any misuse of APIs or tools on this script
+
+positional arguments:
+  IP                   IP address that is going to be searched
+
+options:
+  -h, --help           show this help message and exit
+  -all                 Uses all tools
+  -geo, --location     Uses IP-api to get IP location (no key needed)
+  -raw                 Displays raw json output
+  -vt, --VirusTotal    Uses VirusTotal API for info (key required -> edit API_keys.txt file or uncomment lines)
+  -abuse, --AbuseIPdb  Uses AbuseIPdb API for info (key required -> edit API_keys.txt file or uncomment lines)
+  -otx, --AlienVault   Uses OTX AlienVault API for info (key required -> edit API_keys.txt file or uncomment lines)
+  -showkeys            display API keys
+  -criminalip          Uses CriminalIP api for info (key required -> edit API_keys.txt file or uncomment lines)
+  -c2, --Feodo         Checks if IP is in Abuse.ch Feodo tracker last 30 days C2 IoCs (no key needed)
+  -threatfox           Uses abuse.ch ThreatFox API for info (no key needed)
+  -gn, --greynoise     Uses GreyNoise API for info (key required -> edit API_keys.txt file or uncomment lines)
+  -nokey               Only uses tools that do not require an API key
+
+https://github.com/pbwbb/Projects/tree/main/IPscalper by Pedro Webber
+```
+## Example output
+
+```
+python IPscalper.py -all 8.8.8.8
+
+    ooOoOOo OooOOo.                      o                    
+       O    O     `O                    O                     
+       o    o      O                    o                     
+       O    O     .o                    O                     
+       o    oOooOO'  .oOo  .oOo  .oOoO' o  .oOo. .oOo. `OoOo. 
+       O    o        `Ooo. O     O   o  O  O   o OooO'  o     
+       O    O            O o     o   O  o  o   O O      O     
+    ooOOoOo o'       `OoO' `OoO' `OoO'o Oo oOoO' `OoO'  o     
+                                           O                  
+                                           o'  by Pedro Webber                
+    
+
+-+-+-+-+-+-+-+-+-+-+
+
+IP location:
+
+country: United States(US)
+region: Virginia(VA)
+city: Ashburn
+zip: 20149
+coordinates: lat: 39.03 lon: -77.5
+timezone: America/New_York
+
+-+-+-+-+-+-+-+-+-+-+
+
+AbuseIPdb:
+
+IP: 8.8.8.8
+Domain: google.com
+hostnames: dns.google
+Abuse Confidence Score: 0
+IP public: True
+IP version: 4
+Whitelisted: True
+
+-+-+-+-+-+-+-+-+-+-+
+
+CriminalIP:
+
+IP: 8.8.8.8
+Malicious: False
+VPN: False
+Remote Access: False
+Open Ports: 3
+	Port: 443 tcp
+	Protocol: https
+
+	Port: 53 udp
+	Protocol: dns
+
+	Port: 53 tcp
+	Protocol: dns
+
+Vulnerabilities: 0 []
+
+-+-+-+-+-+-+-+-+-+-+
+
+VirusTotal analisys:
+
+IP: 8.8.8.8 
+AS Owner: GOOGLE
+votes: 
+	harmless: 186
+	malicious: 29
+Vendor: ArcSight Threat Intelligence
+Category: malicious
+Result: malware
+
+-+-+-+-+-+-+-+-+-+-+
+
+OTX AlienVault:
+
+IP: 8.8.8.8
+Reputation: 0
+Pulses: 0
+
+-+-+-+-+-+-+-+-+-+-+
+
+FEODO c2 tracker by abuse.ch:
+
+Not found on the past 30 days IOCs
+
+-+-+-+-+-+-+-+-+-+-+
+
+ThreatFox by abuse.ch:
+
+Your search did not yield any results
+
+-+-+-+-+-+-+-+-+-+-+
+
+GreyNoise:
+
+GreyNoise: 8.8.8.8
+Name: Google Public DNS
+This IP is part of our RIOT project, which identifies IPs from known benign services and organizations that commonly cause false positives in network security and threat intelligence products.
+
+-+-+-+-+-+-+-+-+-+-+
+
+```
+
+## Disclaimer
+
+I am not responsible for any misuse of APIs or tools used in this script. Use responsibly and respect API usage limits.
+
+
+---
+
+[GitHub Repository](https://github.com/pedrowebber/IPscalper) by Pedro Webber
